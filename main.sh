a268af1d5a648067a1ce9fdb655a2bad2a8cf204
bachmt28 <58168531+bachmt28@users.noreply.github.com>
2019-11-27 16:00:55 +0700

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,26 @@
+#jenkins quiet period feature
+
+source var.sh
+source function.sh
+
+#Check prerequire
+
+
+##main
+case $action in
+    add )
+        V_natport_add && V_natport_edit && V_natport_ena
+		echo "$profilename;date +%d-%m-%Y;$EndDate" >> $WSP/listnatport.csv
+		;;
+    delete )
+        V_natport_del
+		;;
+    edit )
+        V_natport_edit ;;
+	enable )
+        V_natport_ena ;;
+    disable )
+        V_natport_dis ;;
+    show )
+        V_natport_show | egrep "$profilename|$private_ip";;
+esac
