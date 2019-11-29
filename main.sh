daec724e6ea88f11b21a0af976eead721db61d88
bachmt28 <58168531+bachmt28@users.noreply.github.com>
2019-11-29 15:09:57 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,6 +4,8 @@ set -e
 set -u 
 
 echo 'This is main part. Script run aldready !'
+
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $DIR/var.sh
 source $DIR/function.sh
