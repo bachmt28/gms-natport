04ed1b7298a83e3e626c58d0027c75a5aa6a8660
bachmt28 <58168531+bachmt28@users.noreply.github.com>
2019-12-02 16:52:30 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,6 +2,7 @@
 #jenkins quiet period feature
 set +e
 set -u 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $DIR/var.sh
 source $DIR/function.sh
 
@@ -13,7 +14,7 @@ echo 'This is main part. Script run aldready !
 '
 
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 
 mkdir -p $WSP/listnatport.csv
 
